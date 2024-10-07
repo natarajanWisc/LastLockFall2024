1f361e4b7886ed109c073bd98e05ce55edbf8bf6
EGaug <144638221+EGaug@users.noreply.github.com>
2024-10-06 23:22:19 -0500

    First add to Eli-branch

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{
+      backgroundColor: '#333',
+      color: 'white',
+      height: '100vh',
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      fontSize: '24px',
+      fontWeight: 'bold'
+    }}>
+      Last Lock Interface
+    </div>
+  );
+}
+
+export default App;
