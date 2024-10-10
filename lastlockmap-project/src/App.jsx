5fc57abaecea6d4b667e4b8a1e73f7caba423ef8
= <=>
2024-10-10 16:44:12 -0500

    initial time series visualization

--- lastlockmap-project/src/App.jsx
+++ lastlockmap-project/src/App.jsx
@@ -1,35 +1,47 @@
-import { useRef, useEffect } from 'react'
-import mapboxgl from 'mapbox-gl'
-
+import { useRef, useEffect } from 'react';
+import mapboxgl from 'mapbox-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
+import './App.css';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Home from './Home';
+import NewPage from './newPage';
 
-import './App.css'
-
-const INITIAL_CENTER = [-89.40811, 43.07189]
+const INITIAL_CENTER = [-89.40811, 43.07189];
 
 function App() {
-
-  const mapRef = useRef()
-  const mapContainerRef = useRef()
+  const mapRef = useRef();
+  const mapContainerRef = useRef();
 
   useEffect(() => {
-    mapboxgl.accessToken = 'pk.eyJ1IjoianVzdGluYmlsZG5lciIsImEiOiJjbTIyM3c0azUwMnllMmxuNzFwY3V0Y204In0.n4K--AHy82A8xh9JxeLUnw'
+    mapboxgl.accessToken = 'pk.eyJ1IjoianVzdGluYmlsZG5lciIsImEiOiJjbTIyM3c0azUwMnllMmxuNzFwY3V0Y204In0.n4K--AHy82A8xh9JxeLUnw';
     mapRef.current = new mapboxgl.Map({
       container: mapContainerRef.current,
       center: INITIAL_CENTER,
-      zoom: 18.2
+      zoom: 18.2,
     });
 
     return () => {
-      mapRef.current.remove()
-    }
-  }, [])
-  
+      mapRef.current.remove();
+    };
+  }, []);
+
   return (
     <>
-      <div id='map-container' ref={mapContainerRef}/>
+      <div id='map-container' ref={mapContainerRef} />
+      <Router>
+        <nav>
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/new-page">New Page</Link></li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/new-page" element={<NewPage />} />
+        </Routes>
+      </Router>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
