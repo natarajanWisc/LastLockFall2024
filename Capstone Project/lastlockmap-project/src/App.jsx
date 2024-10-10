fe9af6d06caf970a1728015750dd1ce2ab1739ea
EGaug <144638221+EGaug@users.noreply.github.com>
2024-10-10 18:10:20 -0500

    Reapply "adding new mapbox implementation and floor selecting"
    This reverts commit fdfbf470c2d5c0564601eb66e25e880548898af9.

---
+++ Capstone Project/lastlockmap-project/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import MapboxContainer from './components/mapboxContainer';
+import './App.css';
+
+function App() {
+  return (
+     <MapboxContainer />
+  );
+}
+
+export default App;
