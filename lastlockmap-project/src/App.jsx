379e037d3a8f7aa5f0e859df664bc367ee6c8e57
juntrecht <untrecht@wisc.edu>
2024-10-22 13:20:14 -0500

    fixed login page styling

--- lastlockmap-project/src/App.jsx
+++ lastlockmap-project/src/App.jsx
@@ -1,19 +1,25 @@
-import React, {useState} from 'react';
-import MapboxContainer from './components/mapboxContainer';
+import React, { useState } from 'react';
+import MapboxContainer from './components/MapboxContainer';
 import LoginPage from './components/LoginPage';
-
+//import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 function App() {
   // State to track if the user is logged in
   const [loggedIn, setLoggedIn] = useState(false);
-  // Function to handle successful login
 
+  // Function to handle successful login
   const handleLogin = (email, password) => {
     // For simplicity, we're assuming login is successful here
     // In a real application, you'd validate login credentials with an API
     console.log('Logged in:', { email, password });
-    setLoggedIn(true); // Set the loggedIn state to true on successful login
+    
+    // You could validate here before setting loggedIn to true
+    if (email === 'admin' && password === 'admin') {
+      setLoggedIn(true);
+    } else {
+      alert("Invalid login credentials. Please try again.");
+    }
   };
 
   return (
@@ -27,6 +33,6 @@ function App() {
       )}
     </div>
   );
-};
+}
 
 export default App;
